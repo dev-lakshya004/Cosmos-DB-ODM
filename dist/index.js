d8c8be8c56a66373d5ce1673fee7ef3c1e31ac67
dev-lakshya004 <lakshya@factacy.ai>
2025-09-11 04:46:42 +0000

    added patch

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-export * from "./db.connection";
-export * from "./lib/QueryBuilder";
-export * from "./lib/BaseModel.odm";
-export const __version = "1.0.0";
+export * from "./db.connection.js";
+export * from "./lib/QueryBuilder.js";
+export * from "./lib/BaseModel.odm.js";
+export const __version = "1.0.1";
 //# sourceMappingURL=index.js.map
